f64c1a968e0419ce26602d5aac56491a4b4935dd
Tobias Grasse <dotdotfx@gmail.com>
2014-12-13 01:08:02 +0100

    Initial commit of version 1.0
    Signed-off-by: Tobias Grasse <dotdotfx@gmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+# minimalMuk
+
+**Most naked & helpful you can get.**
+
+WordPress theme loosely based on [Starkers](http://starkerstheme.com/) with a lot of comments for fresh students of [media & communication science](http://www.medienkomm.uni-halle.de/) at the [Martin Luther University Halle-Wittenberg](http://www.uni-halle.de) to learn the basics of HTML, CSS & WordPress.
+
+For the sake of simplification this theme has a lot of features removed. Using this on your own site probably isn't a good idea unless you want to teach the very basics of WordPress theme development.
+
+To allow adding a site header image via the backend, some CSS is written in the head style area. The rest is managed directly via the style.css.
+
+## Credits
+
+This theme was originally created by [Matthias Kretschmann](http://matthiaskretschmann.com). Further development and current maintenance is done by [Tobias Grasse](https://tobias-grasse.net).
+
+
+## License
+
+GPL, do whatever pleases you.
---
+++ mm-devtools.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: minimalmuk DevTools
+Plugin URI: http://mukblogs.medienkomm.uni-halle.de
+GitHub Plugin URI: https://github.com/muk-mlu-multimedia/mm-devtools
+Description: Adds an admin bar to toggle visual CSS debugging and a 960 grid overlay. Grid overlay adapted from http://webdesign.tutsplus.com/tutorials/htmlcss-tutorials/quick-tip-a-handy-grid-overlay-toolbar/
+Author: Tobias Grasse
+Version: 1.0
+Author URI:
+License: GPL2
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+function dev_assets() {
+  echo  '<!-- Temporary Development Assets -->';
+  echo	'<!-- ******************************************************** -->';
+  echo	'<link rel="stylesheet" href="'. plugin_dir_url(__FILE__) .'mm-devtools.css">';
+  echo	'<script src="http://code.jquery.com/jquery-latest.js"></script>';
+  echo	'<script src="'. plugin_dir_url(__FILE__) .'mm-devtools.js"></script>';
+  echo	'<!-- ******************************************************** -->';
+}
+
+add_action('wp_footer', 'dev_assets');
+
+?>
