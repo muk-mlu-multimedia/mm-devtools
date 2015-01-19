c8bdfbe231fa52978eb3f9e174b2076ec3a96044
Tobias Grasse <dotdotfx@gmail.com>
2015-01-20 00:56:22 +0100

    Fixed syntax error in jquery selector line 23
    Also some JSLint coding style recommendations for spacing.
    
    Signed-off-by: Tobias Grasse <dotdotfx@gmail.com>

--- mm-devtools.js
+++ mm-devtools.js
@@ -1,12 +1,12 @@
 // additional sources: Paul Quinn http://webdesign.tutsplus.com/tutorials/htmlcss-tutorials/quick-tip-a-handy-grid-overlay-toolbar/#comment-1119320169
 
-$(document).ready(function(){
+$(document).ready(function () {
 
   // Create the development bar and grid classes
   var $developmentBar = $('<ul></ul>', {
       class: 'development-bar',
       // addition
-      html: 
+      html:
         '<li id="development-link-cssdebug">CSS Classes</li>' +
         '<li id="development-link-region-colors">Region Colors</li>' +
         '<li id="development-link-grid-skeleton">Skeleton Grid - 960px</li>'
@@ -20,20 +20,20 @@ $(document).ready(function(){
   $('body').prepend($developmentBar, $960SkeletonGrid);
 
   // Toggle the active state when a button is clicked
-  $('li[id^="development-link-"').click(function(){
+  $('li[id^="development-link-"]').click(function () {
     $(this).toggleClass('active');
   });
 
   // Add data attributes to toggle cssdebug and regioncolor functions
-  $('#development-link-cssdebug').click(function(){
-    $('body').attr('data-cssdebug', $('body').attr('data-cssdebug') == 'enabled' ? 'disabled' : 'enabled')
+  $('#development-link-cssdebug').click(function () {
+    $('body').attr('data-cssdebug', $('body').attr('data-cssdebug') == 'enabled' ? 'disabled' : 'enabled');
   });
 
-  $('#development-link-region-colors').click(function(){
-    $('body').attr('data-regioncolors', $('body').attr('data-regioncolors') == 'enabled' ? 'disabled' : 'enabled')
+  $('#development-link-region-colors').click(function () {
+    $('body').attr('data-regioncolors', $('body').attr('data-regioncolors') == 'enabled' ? 'disabled' : 'enabled');
   });
 
-  $('#development-link-grid-skeleton').click(function(){
+  $('#development-link-grid-skeleton').click(function () {
     // Calculate document height and apply it to the grid container
     $docHeight = $('body').outerHeight() + 'px';
     $('.development-skeleton-960-grid').height($docHeight);
--- mm-devtools.php
+++ mm-devtools.php
@@ -5,7 +5,7 @@ Plugin URI: http://mukblogs.medienkomm.uni-halle.de
 GitHub Plugin URI: https://github.com/muk-mlu-multimedia/mm-devtools
 Description: Adds an admin bar to toggle visual CSS debugging and a 960 grid overlay. Grid overlay adapted from http://webdesign.tutsplus.com/tutorials/htmlcss-tutorials/quick-tip-a-handy-grid-overlay-toolbar/
 Author: Tobias Grasse
-Version: 1.0
+Version: 1.0.1
 Author URI:
 License: GPL2
 */
